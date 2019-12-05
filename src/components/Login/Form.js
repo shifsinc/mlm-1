a71c0f78a6b4332c4618ca3c983b6d878c59a173
YR <admin@01.local>
2019-12-06 02:23:55 +0300

    Handling of input[type="file"] is added in Form.js

--- src/components/Login/Form.js
+++ src/components/Login/Form.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
-function SignInView(props){/*formTitle, submitTitle, submitLink, submitCallback*/
+function SignInView(props){/*formTitle, submitTitle, submitLink, submitCallback, className*/
   var form, error;
   return (
-    <form className="form-view interface-block login-view" ref={r => form = r}>
+    <form className={ 'form-view interface-block login-view' + (props.className ? ' ' + props.className : '') } ref={r => form = r}>
       <div className="error" ref={r => error = r}>{ props.formError ? props.formError: '' }</div>
       { props.formTitle ? <h3 className="form-view__title">{ props.formTitle }</h3> : undefined }
       <div className="form-view__cont">
         { props.children }
         <a className="link button login-view__button" href={ props.submitLink }
           onClick={e => {
+
             e.preventDefault();
             var data = {};
-            Array.prototype.forEach.call( form.querySelectorAll('input'), inp => data[ inp.name ] = inp.value);
+            Array.prototype.forEach.call( form.querySelectorAll('input'), inp => {
+              if(inp.type == 'file') data[ inp.name ] = inp.files.length ? inp.files[0].slice() : null;
+              else data[ inp.name ] = inp.value
+            });
             props.submitCallback( data ).then(r => {
               window.history.pushState(null, '', props.submitLink);
               props.updateLocation();
@@ -21,6 +25,7 @@ function SignInView(props){/*formTitle, submitTitle, submitLink, submitCallback*
               if(err.fields) err.fields.forEach(f => form.querySelector('input[name="' + f + '"]').classList.add('incorrect'));
               form.classList.add('show-error');
             });
+
           }}>{ props.submitTitle }</a>
       </div>
     </form>
