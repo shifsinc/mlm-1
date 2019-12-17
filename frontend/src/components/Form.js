11099c9aad38ef5d3fe45ae1636ae950c4d0ea23
YR <admin@01.local>
2019-12-17 04:18:21 +0300

    Form.js

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -4,36 +4,56 @@ import './Form.css';
 export default function(props){/*formTitle, submitTitle, submitCallback, className*/
   var form, message,
 
+    checkRepeat = inputs => {
+      var ret = true;
+      Array.prototype.forEach.call( inputs, inp => {
+        var repeat = /^(.+)_repeat$/.exec( inp.name );
+        if( !repeat ) return;
+        var field = form.querySelector( 'input[name="' + repeat[1] + '"]' );
+        if( !field ) return;
+        if( inp.value !== field.value ){
+          inp.classList.add('incorrect');
+          ret = false;
+        }
+      });
+      return ret;
+    },
+
     onSubmit = e => {
       e.preventDefault();
-      var data = {};
       if( form.querySelector('input.incorrect') ) return;
-      Array.prototype.forEach.call( form.querySelectorAll('input'), inp => {
+
+      var inputs = form.querySelectorAll('input');
+      if( !checkRepeat(inputs) ) return;
+
+      var data = {};
+      Array.prototype.forEach.call( inputs, inp => {
         if(inp.type === 'file') data[ inp.name ] = inp.files.length ? inp.files[0].slice() : null;
         else data[ inp.name ] = inp.value;
       });
-      props.submitCallback( data ).then(({ action }) => {/*text, fields, path*/
+
+      var prom = props.submitCallback( data );
+      if( !prom ) return;
+      prom.then(({ action }) => {/*text, fields, path*/
         if( !action ) return;
         if( action.text ){
           message.innerHTML = action.text;
           form.classList.add('show-message');
         }
         if( action.fields ) action.fields.forEach(f => form.querySelector('input[name="' + f + '"]').classList.add('incorrect'));
-        if( action.path ){
-          props.updateLocation( action.path );
-        }
+        if( action.path ) props.updateLocation( action.path );
       });
     }
 
   return (
-    <form className={ 'form interface-block' + (props.className ? ' ' + props.className : '') } ref={r => form = r}>
+    <form className={ 'form' + (props.className ? ' ' + props.className : '') } ref={r => form = r}>
       <div className="message" ref={r => message = r}></div>
       { props.formTitle ? <h3 className="form__title">{ props.formTitle }</h3> : undefined }
       <div className="form__cont">
         { props.children }
         <a className="link button form__button" href="##"
           onClick={ onSubmit }>{ props.submitTitle }</a>
-        <input type="submit" onClick={ onSubmit } style={{ display: 'none' }}></input>
+        <button onClick={ onSubmit } style={{ display: 'none' }}></button>
       </div>
     </form>
   );
