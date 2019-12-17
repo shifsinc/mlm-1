94c32c45c8784196f19baf4ab89d991af7e4d5ed
YR <admin@01.local>
2019-12-17 16:05:49 +0300

    Links.js

--- frontend/src/components/Main/Account/Links.js
+++ frontend/src/components/Main/Account/Links.js
@@ -1,10 +1,51 @@
 import React from 'react';
 import './Links.css'
+import Block from './Block.js'
+import Input from '../../Input.js'
+import { domain } from '../../../config.js'
 
-export default function(props) {/*updateLocation*/
-  return (
-    <div className="account__links interface-block">
-
-    </div>
-  );
+export default class extends React.Component {
+  constructor(props) {/*updateLocation, dataSrc*/
+    super(props);
+    this.state = {
+      phone: '',
+      general_type: 'l'
+    }
+    props.dataSrc.then(res => {
+      this.setState({ phone: res.user_phone, general_type: res.general_link_type });
+    })
+  }
+  render(){
+    var link = 'https://' + domain + '/signup',
+      setTarget = type => {
+        if(type === this.state.general_type) return;
+        this.props.apiCall('setGeneralLinkType', { type }).then( r => {
+          if(r.status === 'ok') this.setState({ general_type: type });
+        })
+      },
+      copyText = input => {
+        input.select();
+        document.execCommand('copy');
+        input.blur();
+      }
+    return (
+      <Block title="Мои ссылки" className="account__links">
+        <div className="account__links_cont">
+        <Input attr={{ readOnly: true, value: link + '?refer=' + this.state.phone + '&type=l' }}
+          label="Ссылка на левую ногу" className="label-top" buttonClassName="account__links__copy-link"
+          buttonClick={e => copyText( e.target.parentNode.querySelector('input') )}></Input>
+        <Input attr={{ readOnly: true, value: link + '?refer=' + this.state.phone + '&type=r'  }}
+          label="Ссылка на правую ногу" className="label-top" buttonClassName="account__links__copy-link"
+          buttonClick={e => copyText( e.target.parentNode.querySelector('input') )}></Input>
+        <Input attr={{ readOnly: true, value: link + '?refer=' + this.state.phone }}
+          label="Общая ссылка" className="label-top" buttonClassName="account__links__copy-link"
+          buttonClick={e => copyText( e.target.parentNode.querySelector('input') )}></Input>
+        <div className="account__links__switch">
+          <div onClick={ () => setTarget('l') } className={ this.state.general_type === 'l' ? 'active': '' }>Левая нога</div>
+          <div onClick={ () => setTarget('r') } className={ this.state.general_type === 'r' ? 'active': '' }>Правая нога</div>
+        </div>
+        </div>
+      </Block>
+    );
+  }
 }
