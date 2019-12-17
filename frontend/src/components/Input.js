5acb8b33715cbbdd5c3e30ff4fb274ae0a95b720
YR <admin@01.local>
2019-12-17 16:05:29 +0300

    Input button added

--- frontend/src/components/Input.js
+++ frontend/src/components/Input.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Input.css';
 
-export default function(props){/*regexp, className, attr, label*/
+export default function(props){/*regexp, className, attr, label, buttonClassName, buttonClick*/
   var _onBlur = e => {
     var targ = e.target;
     if( targ.value && props.regexp && !props.regexp.test( targ.value ) ) targ.classList.add('incorrect');
@@ -11,6 +11,8 @@ export default function(props){/*regexp, className, attr, label*/
     <div className={ 'input' + (props.className ? ' ' + props.className : '') }>
       <input required {...props.attr} onBlur={ _onBlur }/>
       <label className="input__label">{ props.label }</label>
+      <div className={ 'input__button' + (props.buttonClassName ? ' ' + props.buttonClassName : '') }
+        onClick={ props.buttonClick ? props.buttonClick : () => {} }></div>
     </div>
   );
 }
