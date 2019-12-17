66fc6fce24ec6278affd28516743cb1318eeb848
YR <admin@01.local>
2019-12-17 04:20:01 +0300

    Menu

--- frontend/src/components/Main/Menu.js
+++ frontend/src/components/Main/Menu.js
@@ -3,23 +3,27 @@ import './Menu.css'
 import Link from '../Link.js'
 
 export default function(props){/*updateLocation*/
-  var commonProps = { updateLocation: props.updateLocation }, loc = props.location;
+  var commonProps = { updateLocation: props.updateLocation }, loc = props.location, menu;
   return (
-    <div id="menu" className="interface-block">
-      <Link { ...commonProps } path="/account" title="Личный кабинет"
-        className={ 'menu__item account-icon' + ( loc === '/account' ? ' active' : '' ) }></Link>
-      <Link { ...commonProps } path="/robot" title="Мой робот"
-        className={ 'menu__item robot-icon' + ( loc === '/robot' ? ' active' : '' ) }></Link>
-      <Link { ...commonProps } path="/team" title="Моя команда"
-        className={ 'menu__item team-icon' + ( loc === '/team' ? ' active' : '' ) }></Link>
-      <Link { ...commonProps } path="/marketing" title="Маркетинг"
-        className={ 'menu__item marketing-icon' + ( loc === '/marketing' ? ' active' : '' ) }></Link>
-      <Link { ...commonProps } path="/finances" title="Финансы"
-        className={ 'menu__item finances-icon' + ( loc === '/finances' ? ' active' : '' ) }></Link>
-      <Link { ...commonProps } path="/instructions" title="Инструкции"
-        className={ 'menu__item instructions-icon' + ( loc === '/instructions' ? ' active' : '' ) }></Link>
-      <Link { ...commonProps } path="/settings" title="Настройки"
-        className={ 'menu__item settings-icon' + ( loc === '/settings' ? ' active' : '' ) }></Link>
+    <div>
+    <div id="menu" className="interface-block main__menu close" ref={ r => menu = r } onClick={ e => menu.classList.add('close') }>
+      <div className="main__menu__mobile" onClick={ e => {e.stopPropagation();menu.classList.remove('close')} }></div>
+      <Link { ...commonProps } path="/account" className={ 'menu__item account-icon' + ( loc === '/account' ? ' active' : '' ) }>
+        Личный кабинет</Link>
+      <Link { ...commonProps } path="/robot" className={ 'menu__item robot-icon' + ( loc === '/robot' ? ' active' : '' ) }>
+        Мой робот</Link>
+      <Link { ...commonProps } path="/team" className={ 'menu__item team-icon' + ( loc === '/team' ? ' active' : '' ) }>
+        Моя команда</Link>
+      <Link { ...commonProps } path="/marketing" className={ 'menu__item marketing-icon' + ( loc === '/marketing' ? ' active' : '' ) }>
+        Маркетинг</Link>
+      <Link { ...commonProps } path="/finances" className={ 'menu__item finances-icon' + ( loc === '/finances' ? ' active' : '' ) }>
+        Финансы</Link>
+      <Link { ...commonProps } path="/instructions" className={ 'menu__item instructions-icon' + ( loc === '/instructions' ? ' active' : '' ) }>
+        Инструкции</Link>
+      <Link { ...commonProps } path="/settings" className={ 'menu__item settings-icon' + ( loc === '/settings' ? ' active' : '' ) }>
+        Настройки</Link>
+    </div>
+    <div className="cover menu-mobile-cover" onClick={ e => menu.classList.add('close') }></div>
     </div>
   );
 }
