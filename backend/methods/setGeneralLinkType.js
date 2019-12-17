6a076b775feb94bcac0dc1d28f53c45d54d43776
YR <admin@01.local>
2019-12-17 15:41:26 +0300

    setGeneralLinkType API

---
+++ backend/methods/setGeneralLinkType.js
@@ -0,0 +1,12 @@
+const { makeQuery } = require('../utils.js');
+const { INCORRECT_QUERY, OK } = require('../const.js');
+
+module.exports = function(callback, params, _user_id){/*type*/
+  var type = params.type;
+  if( type !== 'l' && type !== 'r' ) return callback( INCORRECT_QUERY );
+
+  makeQuery(`UPDATE users SET general_link_type=? WHERE user_id=?`, [ type, _user_id ],
+    res => {
+      callback( OK );
+    }, callback);
+}
