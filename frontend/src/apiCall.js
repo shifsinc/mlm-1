730139a0c93e6693fced0d116978870373b6844e
YR <admin@01.local>
2019-12-17 04:22:03 +0300

    config added

--- frontend/src/apiCall.js
+++ frontend/src/apiCall.js
@@ -1,9 +1,10 @@
+import { apiEndpoint } from './config.js'
+
 export default (method, token, data = {}) => {
-  return fetch('http://localhost:8081/' + method, {
-  //return fetch('http://mlm.dig-studio.ru:8081/' + method, {
+  return fetch(apiEndpoint + method, {
       method: 'POST',
       body: JSON.stringify({ ...data, token })
   })
   .then(r => r.json())
-  .catch(r => {return { status: 'error', text: 'Request error' }});
+  .catch(r => { return { status: 'error', text: 'Request error', action: { text: 'Ошибка запроса' } } });
 }
---
+++ frontend/src/config.js
@@ -0,0 +1,2 @@
+exports.apiEndpoint = 'http://01.local:8081/';
+//exports.apiEndpoint = 'http://mlm.dig-studio.ru:8081/';
