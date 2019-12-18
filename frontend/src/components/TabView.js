55283c93049cebce8def95814f06903ad64301eb
YR <admin@01.local>
2019-12-18 16:43:44 +0300

    TabView

--- frontend/src/components/TabView.js
+++ frontend/src/components/TabView.js
@@ -3,14 +3,16 @@ import './TabView.css'
 import Link from './Link.js'
 
 export default function(props){/*updateLocation, titles*/
-  var cont, lastActive = 0;
+  var cont, header, lastActive = 0;
   return (
     <div className={ 'tab-view' + ( props.className ? ' ' + props.className : '') }>
-    <div className="tab-view__header">
+    <div className="tab-view__header" ref={ r => {header = r; if(r) r.children[0].classList.add('active')} }>
       { props.titles.map((t, i) =>
           <Link className="tab-view__header__item" updateLocation={ props.updateLocation } onClick={ () => {
             cont.children[ lastActive ].classList.remove('active');
             cont.children[ i ].classList.add('active');
+            header.children[ lastActive ].classList.remove('active');
+            header.children[ i ].classList.add('active');
             lastActive = i;
           } }>{ t }</Link>
         ) }
