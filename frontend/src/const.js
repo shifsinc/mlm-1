a75349bf17abdaeefd3a2cf39e014dd12cac03ff
YR <admin@01.local>
2019-12-19 03:32:04 +0300

    ethereum, paypal regexp

--- frontend/src/const.js
+++ frontend/src/const.js
@@ -1,7 +1,8 @@
 exports.loginRegexp = /^\w{5,30}$/;
-exports.emailRegexp = /^.+@[a-zA-Z\-0-9.]+$/;
+exports.emailRegexp = exports.paypalRegexp = /^.+@[a-zA-Z\-0-9.]+$/;
 exports.passwordRegexp = /(?=^.{8,30}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
 exports.nameRegexp = /^[\wа-яА-Я]+$/;
 exports.phoneRegexp = /^[0-9]{11,15}$/;
 exports.linkRegexp = /^https?:\/\/[a-zA-Z\-0-9.]+(\/.*)?$/;
 exports.telegramRegexp = /^\w+$/;
+exports.ethereumRegexp = /^0x[a-z0-9]{40}$/;
