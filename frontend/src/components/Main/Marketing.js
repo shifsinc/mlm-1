203bdf0880893ee558d079e1e93962811868caed
YR <admin@01.local>
2019-12-19 03:32:17 +0300

    Marketing

--- frontend/src/components/Main/Marketing.js
+++ frontend/src/components/Main/Marketing.js
@@ -3,11 +3,10 @@ import './Marketing.css'
 import FilesView from './common/FilesView.js'
 
 export default function(props) {/*updateLocation*/
-  var checkResult = r => r.result ? r.result : [];
   return (
     <div className="main__content">
-      <FilesView header="Маркетинг"
-        dataSrc={ props.apiCall('getFiles', { section: 'marketing' }).then( checkResult ) }></FilesView>
+      <FilesView title="Маркетинг"
+        dataSrc={ props.apiCall('getFiles', { section: 'marketing' }).then(r => r.result ? r.result : []) }></FilesView>
     </div>
   );
 }
