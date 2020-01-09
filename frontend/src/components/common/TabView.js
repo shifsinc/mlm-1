b011d5bc65cbaa472a93de7fd65f9a76d9b8cab4
YR <admin@01.local>
2020-01-09 03:38:00 +0300

    styles

--- frontend/src/components/common/TabView.js
+++ frontend/src/components/common/TabView.js
@@ -15,14 +15,17 @@ export default class extends React.Component {
     return (
       <div className={ 'tab-view ' + ( this.props.className ? this.props.className : '') }>
         <div className="tab-view__header closed" ref={ r => this.header = r } onClick={ () => this.header.classList.add('closed') }>
-          { this.props.titles.map((text, ind) =>
+          <div className="tab-view__header__cont">
+            { this.props.titles.map((text, ind) =>
 
-              <Link key={ ind } className={ 'tab-view__header__item ' + ( this.state.activeTab === ind ? 'active' : '' ) }
-                onClick={ () => this.setState({ activeTab: ind }) }>{ text }</Link>
+                <Link key={ ind } className={ 'tab-view__header__item ' + ( this.state.activeTab === ind ? 'active' : '' ) }
+                  onClick={ () => this.setState({ activeTab: ind }) }>{ text }</Link>
 
-            ) }
-            <div className="tab-view__header_mobile"
-              onClick={ e => {this.header.classList.remove('closed');e.stopPropagation()} }></div>
+              ) }
+              <div className="tab-view__header_mobile"
+                onClick={ e => {this.header.classList.remove('closed');e.stopPropagation()} }></div>
+            </div>
+            <div className="cover tab-view__cover-mobile" onClick={ e => this.header.classList.add('closed') }></div>
         </div>
         <div className="tab-view__content">
           <ViewSelect active={ this.state.activeTab }>
