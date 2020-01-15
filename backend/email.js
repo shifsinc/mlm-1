3d666cbec8fa85b5f791e20800a6fe3b21d8f590
YR <admin@01.local>
2020-01-16 02:16:21 +0300

    email html

--- backend/email.js
+++ backend/email.js
@@ -1,10 +1,15 @@
 const { sendMail } = require('./utils.js');
 const { DOMAIN } = require('./config.js');
+const template = require('./email-template.js');
 
 exports.sendConfirmMail = function(receiver, token, callback){
-  sendMail(receiver, 'Please, confirm your email address https://' + DOMAIN + '/confirmEmail?confirmToken=' + token, callback);
+  const html = template(`<h2>Пожалуйста, подтвердите ваш адрес электронной почты</h2>
+    <a class="link button" target="_blank" href="https://${DOMAIN}/confirmEmail?confirmToken=${token}">Подтвердить</a>`);
+  sendMail(receiver, html, callback);
 }
 
 exports.sendResetMail = function(receiver, token, callback){
-  sendMail(receiver, 'Reset password https://' + DOMAIN + '/passwordReset?resetToken=' + token, callback);
+  const html = template(`<h2>Восстановление пароля</h2>
+    <a class="link button" target="_blank" href="https://${DOMAIN}/passwordReset?resetToken=${token}">Восстановить</a>`);
+  sendMail(receiver, html, callback);
 }
