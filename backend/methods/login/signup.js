20fc24a5a4c46aadd1f1879ae6fb53180eecb6ac
YR <admin@01.local>
2020-01-18 00:05:55 +0300

    removed possibility to register without refer

--- backend/methods/login/signup.js
+++ backend/methods/login/signup.js
@@ -1,5 +1,5 @@
 const { makeQuery, beginTransaction } = require('../../utils.js');
-const { INCORRECT_QUERY, OK, loginRegexp, passwordRegexp, emailRegexp, phoneRegexp } = require('../../const.js');
+const { INCORRECT_QUERY, OK, loginRegexp, passwordRegexp, emailRegexp, phoneRegexp, USER_NOT_EXISTS } = require('../../const.js');
 const { sendConfirmMail } = require('../../email.js');
 const hash = require('js-sha1');
 
@@ -8,10 +8,9 @@ module.exports = function(callback, params){/*login, password, email, refer_phon
     refer = params.refer_phone, refer_type = params.refer_type;
   if( login === undefined || !loginRegexp.test(login) ||
       password === undefined || !passwordRegexp.test(password) ||
-      email === undefined || !emailRegexp.test(email) )
+      email === undefined || !emailRegexp.test(email) ||
+      refer === undefined || !phoneRegexp.test(refer) )
     return callback( INCORRECT_QUERY );
-
-  if( refer === undefined || !phoneRegexp.test(refer) ) refer = '*';
 
   makeQuery(`SELECT user_id FROM users WHERE user_login=? OR user_email=?`, [ login, email ],
     res => {
@@ -23,10 +22,11 @@ module.exports = function(callback, params){/*login, password, email, refer_phon
 
           var query, values, refer_id = null, confirmToken = hash( Math.random() + '' );
           if(!res.result.length){
-            query = `INSERT INTO users(user_login, user_password_hash, user_email, email_confirm_token)
+            return callback( USER_NOT_EXISTS );
+            /*query = `INSERT INTO users(user_login, user_password_hash, user_email, email_confirm_token)
               VALUES(?,SHA(?),?,?)`;
             values = [ login, password, email, confirmToken ];
-            refer_type = null;
+            refer_type = null;*/
           } else {
             query = `INSERT INTO users(user_login, user_password_hash, user_email, user_refer, user_refer_type, email_confirm_token)
               VALUES(?,SHA(?),?,?,?,?)`;
