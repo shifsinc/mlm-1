e6ad7e67fec0562c79b7390727c6c6b378becc3f
YR <admin@01.local>
2020-01-17 23:14:33 +0300

    getReferals line filter added

--- backend/methods/get/getReferals.js
+++ backend/methods/get/getReferals.js
@@ -1,24 +1,26 @@
 const { makeQuery } = require('../../utils.js');
-const { INCORRECT_QUERY, OK } = require('../../const.js');
+const { INCORRECT_QUERY, OK, NO_DATA_PAGES } = require('../../const.js');
 const { PHOTOS_PREFIX } = require('../../config.js');
 
-module.exports = function(callback, params, _user_id){/*count, offset, pattern*/
-  var count = parseInt( params.count ), offset = parseInt( params.offset ), pattern = params.pattern ? params.pattern + '%' : '%';
+module.exports = function(callback, params, _user_id){/*count, offset, pattern, line*/
+  var count = parseInt( params.count ), offset = parseInt( params.offset ),
+    pattern = params.pattern ? params.pattern + '%' : '%', line = parseInt( params.line );
   if( isNaN(offset)  || isNaN(count) || count < 1 || count > 20 ) return callback( INCORRECT_QUERY );
+  if( isNaN( line ) || line < 1 && line > 2 ) line = null;
 
   var sync = 0, left_ids, right_ids;
   const hnd = () => {
     if( sync ){
 
       var result_ids = left_ids.concat( right_ids );
-      if( !result_ids.length ){
-        var res = OK;
-        res.result = {
-        count: 0,
-        data: []
+      if( !result_ids.length ) return callback( NO_DATA_PAGES );
+
+      var lineCond = '';
+      if( line === 1 ){
+        lineCond = 'AND user_refer=' + _user_id;
+      } else if( line === 2 ){
+        lineCond = 'AND user_refer<>' + _user_id;
       }
-      return callback( res );
-    }
 
       makeQuery(`SELECT
         user_id,
@@ -29,22 +31,25 @@ module.exports = function(callback, params, _user_id){/*count, offset, pattern*/
         user_dt,
         user_rate + 0 AS user_rate
         FROM users
-        WHERE user_id IN(?) AND user_login LIKE ?
+        WHERE user_id IN(?) AND user_login LIKE ? ${lineCond}
         ORDER BY user_dt
         LIMIT ?,?`, [ result_ids, pattern, offset, count ],
         res => {
 
-          makeQuery(`SELECT COUNT(*) AS count FROM users WHERE user_id IN(?) AND user_login LIKE ?`, [ result_ids, pattern ],
+          makeQuery(`SELECT COUNT(*) AS count FROM users WHERE user_id IN(?) AND user_login LIKE ? ${lineCond}`,
+            [ result_ids, pattern ],
             count => {
 
+              var response = [];
               res.result.forEach(r => {
                 r._user_direction = left_ids.includes( r.user_id ) ? 'l' : 'r';
                 r._is_team = r.user_refer === _user_id ? true : false;
                 if( r.user_photo ) r.user_photo_url = PHOTOS_PREFIX + r.user_photo;
+                if( line === null || ( line === 1 && r._is_team ) || ( line === 2 && !r._is_team ) ) response.push(r);
               });
               res.result = {
                 count: count.result[0].count,
-                data: res.result
+                data: response
               }
               callback(res);
 
