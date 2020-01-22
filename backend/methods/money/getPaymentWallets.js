05b8cf4ad9e7acaee8fe18ead975cc4b75d2243f
YR <admin@01.local>
2020-01-22 18:22:00 +0300

    new eth account

--- backend/methods/money/getPaymentWallets.js
+++ backend/methods/money/getPaymentWallets.js
@@ -2,7 +2,7 @@ const { makeQuery } = require('../../utils.js');
 
 module.exports = function(callback, params, _user_id){/**/
   callback({ status: 'ok', result: {
-    eth_wallet: 'bnb136ns6lfw4zs5hg4n85vdthaad7hq5m4gtkgf23',
+    eth_wallet: '0x373856e432f80d8170573284579c84be3d488a1e',
     paypal_wallet: 'vitalycool@gmail.com'
   } });
 }
