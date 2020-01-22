098132e49f102e84f0a68ea84f29effee15fe93d
YR <admin@01.local>
2020-01-22 19:48:21 +0300

    ...

--- frontend/src/components/Main/common/VideosView.js
+++ frontend/src/components/Main/common/VideosView.js
@@ -8,12 +8,13 @@ export default function(props){/*data, _admin, _editClick, _deleteClick*/
   var className = '';
   if( data.length > 1 ) className = 'multiple';
   return data.map( (d, i) => {
-    return (<div className="videos-view__video">{
-      props._admin ? <div className="admin__controls">
-          <Link className="admin__edit-button" onClick={ () => props.editClick(d) }></Link>
-          <Link className="admin__delete-button" onClick={ () => props._deleteClick(d)}></Link>
-        </div> : undefined
-    }
+    return (<div className="videos-view__video">
+    { props._admin ?
+      <div className="admin__controls">
+        <Link className="admin__edit-button" onClick={ () => props._editClick(d) }></Link>
+        <Link className="admin__delete-button" onClick={ () => props._deleteClick(d)}></Link>
+      </div>
+      : undefined }
     <div className={ 'videos-view__video__cont ' + className } dangerouslySetInnerHTML={{ __html: d.file_descr }}></div>
     </div>);
   });
