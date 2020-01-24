e3d0ec116373f3968be543bb89d028af4086f257
YR <admin@01.local>
2020-01-24 22:51:32 +0300

    telegram not required

--- backend/methods/login/fillUserInfo.js
+++ backend/methods/login/fillUserInfo.js
@@ -1,5 +1,5 @@
 const { makeQuery } = require('../../utils.js');
-const { INCORRECT_QUERY, FORBIDDEN, DATA_NOT_UNIQUE, NO_PHOTO,
+const { INCORRECT_QUERY, OK, FORBIDDEN, DATA_NOT_UNIQUE,
     nameRegexp, phoneRegexp, linkRegexp, telegramRegexp, filenameRegexp } = require('../../const.js');
 const { PHOTOS_PATH } = require('../../config.js');
 
@@ -7,46 +7,46 @@ const { existsSync } = require('fs');
 const readChunk = require('read-chunk');
 const fileType = require('file-type');
 
+const NO_PHOTO_LOADED = { status: 'error', action: { text: 'Пожалуйста, выберите фото' } }
+
 module.exports = function(callback, params, _user_id){/*name, surname, phone, social_link, telegram, photo*/
   var name = params.name, surname = params.surname,
     phone = params.phone, social = params.social_link, telegram = params.telegram, photo = params.photo;
   if( name === undefined || !nameRegexp.test(name) ||
     surname === undefined || !nameRegexp.test(surname) ||
     phone === undefined || !phoneRegexp.test(phone) ||
-    social === undefined || !linkRegexp.test(social) ||
-    telegram === undefined || !telegramRegexp.test(telegram))
+    social === undefined || !linkRegexp.test(social) )
     return callback( INCORRECT_QUERY );
 
-  if( photo !== undefined && filenameRegexp.test(photo) ){
-    var filepath = PHOTOS_PATH + photo;
-    if( existsSync( filepath ) ){
-      var buf = readChunk.sync(filepath, 0, fileType.minimumBytes);
-      filetype = fileType( buf );
-      if( !filetype || !filetype.mime.startsWith('image') ) photo = NO_PHOTO;
-    } else photo = NO_PHOTO;
-  } else photo = NO_PHOTO;
+  if( telegram === undefined || !telegramRegexp.test(telegram) ) telegram = null;
 
-    makeQuery(`SELECT user_data_filled FROM users WHERE user_id=?`, [ _user_id ],
-      res => {
-        if( res.result[0].user_data_filled ) return callback( FORBIDDEN );
+  var filepath = PHOTOS_PATH + photo;
+  if( photo === undefined || !filenameRegexp.test(photo) || !existsSync( filepath ) ) return callback( NO_PHOTO_LOADED );
+  var buf = readChunk.sync(filepath, 0, fileType.minimumBytes);
+  filetype = fileType( buf );
+  if( !filetype || !filetype.mime.startsWith('image') ) return callback( NO_PHOTO_LOADED );
 
-        makeQuery(`UPDATE users SET
-            user_name=?,
-            user_surname=?,
-            user_phone=?,
-            user_social=?,
-            user_telegram=?,
-            user_photo=?,
-            user_data_filled=1
-            WHERE user_id=?`,
-          [ name, surname, phone, social, telegram, photo,  _user_id ],
-          res => {
-            var resp = Object.assign({}, OK, { action: { path: '/account' } });
-            callback(resp);
-          }, () => {
-            callback( DATA_NOT_UNIQUE );
-          });
+  makeQuery(`SELECT user_data_filled FROM users WHERE user_id=?`, [ _user_id ],
+    res => {
+      if( res.result[0].user_data_filled ) return callback( FORBIDDEN );
 
-      }, callback);
+      makeQuery(`UPDATE users SET
+          user_name=?,
+          user_surname=?,
+          user_phone=?,
+          user_social=?,
+          user_telegram=?,
+          user_photo=?,
+          user_data_filled=1
+          WHERE user_id=?`,
+        [ name, surname, phone, social, telegram, photo,  _user_id ],
+        res => {
+          var resp = Object.assign({}, OK, { action: { path: '/account' } });
+          callback(resp);
+        }, () => {
+          callback( DATA_NOT_UNIQUE );
+        });
+
+    }, callback);
 
 }
