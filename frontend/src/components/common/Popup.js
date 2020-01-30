aac3ceee15ffba0ac4c50987d1612577db1d231b
YR <admin@01.local>
2020-01-30 19:42:57 +0300

    popup mousedown close intead click

--- frontend/src/components/common/Popup.js
+++ frontend/src/components/common/Popup.js
@@ -6,7 +6,7 @@ export default function(props){/*className, onClose*/
   var onClose = () => props.onClose && props.onClose();
   return (
     <div className="cover popup__cover" ref={ r => cover = r }
-      onClick={ e => { if( e.target === cover ) onClose() } }>
+      onMouseDown={ e => { if( e.target === cover ) onClose() } }>
       <div className={ 'interface-block popup ' + (props.className ? props.className : '') }>
         <div className="popup__close" onClick={ onClose }></div>
         { props.children }
