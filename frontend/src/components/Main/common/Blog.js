c37a67ef0450d66f85a738d5dab9bd7cbc378bf1
YR <admin@01.local>
2020-02-04 18:06:33 +0300

    news and blog show more

--- frontend/src/components/Main/common/Blog.js
+++ frontend/src/components/Main/common/Blog.js
@@ -7,6 +7,8 @@ import { formatDate } from '../../../utils.js'
 import Link from '../../common/Link.js'
 import '../Admin/common.css'
 
+const BLOG_VISIBLE_LENGTH = 500;
+
 export default function(props){/*apiCall, _admin, _editClick, _deleteClick, _rand*/
   var method = props._admin ? 'admin/getNews' : 'getNews';
   return (<PageView component={ _view } componentProps={{ ...props }} onPageCount={ 5 }
@@ -17,24 +19,39 @@ export default function(props){/*apiCall, _admin, _editClick, _deleteClick, _ran
 function _view(props){
   var data = props.data ? props.data : [];
   return (<div className="blog">
-      {
-        data.map((d, i) => {
-          return (<div key={ i } className="blog__item interface-block">
-            {
-              props._admin ? <div className="admin__controls">
-                  <Link className="admin__edit-button" onClick={ () => props._editClick(d) }></Link>
-                  <Link className="admin__delete-button" onClick={ () => props._deleteClick(d) }></Link>
-                </div> : undefined
-            }
-            <div className="blog__item__title">{ d.news_title }</div>
-            <div className="blog__item__date">
-              { formatDate(new Date( d.news_dt ), false) }
-            </div>
-            <div className="blog__item__text">{ d.news_text }</div>
-            <ImagesView data={ d.images }></ImagesView>
-            <VideosView data={ d.videos }></VideosView>
-          </div>);
-        })
-      }
+      { data.map((d, i) => <BlogItem key={ i } item={ d } { ...props }/>) }
     </div>);
+}
+
+class BlogItem extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      opened: props.item.news_text.length > BLOG_VISIBLE_LENGTH ? false : true
+    }
+  }
+
+  render(){
+    var d = this.props.item;
+
+    return (<div className="blog__item interface-block">
+      {
+        this.props._admin ? <div className="admin__controls">
+            <Link className="admin__edit-button" onClick={ () => this.props._editClick(d) }></Link>
+            <Link className="admin__delete-button" onClick={ () => this.props._deleteClick(d) }></Link>
+          </div> : undefined
+      }
+      <div className="blog__item__title">{ d.news_title }</div>
+      <div className="blog__item__date">
+        { formatDate(new Date( d.news_dt ), false) }
+      </div>
+      <div className={ 'blog__item__text show-more-text' + ( this.state.opened ? '_opened' : '' ) }>
+        <span>{ this.state.opened ? d.news_text : d.news_text.slice(0, BLOG_VISIBLE_LENGTH) + '...' }</span>
+        <div className="show-more" onClick={ () => this.setState({ opened: true }) }></div>
+      </div>
+      <ImagesView data={ d.images }></ImagesView>
+      <VideosView data={ d.videos }></VideosView>
+    </div>);
+  }
 }
--- frontend/src/components/Main/common/News.js
+++ frontend/src/components/Main/common/News.js
@@ -7,6 +7,8 @@ import { formatDate } from '../../../utils.js'
 import Link from '../../common/Link.js'
 import '../Admin/common.css'
 
+const NEWS_VISIBLE_LENGTH = 350;
+
 export default function(props){/*apiCall, _admin, _editClick, _deleteClick, _rand*/
   var method = props._admin ? 'admin/getNews' : 'getNews';
   return (<PageView component={ _view } componentProps={{ ...props }} onPageCount={ 5 }
@@ -17,24 +19,38 @@ export default function(props){/*apiCall, _admin, _editClick, _deleteClick, _ran
 function _view(props){
   var data = props.data ? props.data : [];
   return (<div className="news items-border">
-      {
-        data.map((d, i) => {
-          return (<div key={ i } className="news__item">
-            {
-              props._admin ? <div className="admin__controls">
-                  <Link className="admin__edit-button" onClick={ () => props._editClick(d) }></Link>
-                  <Link className="admin__delete-button" onClick={ () => props._deleteClick(d) }></Link>
-                </div> : undefined
-            }
-            <h4 className="news__item__title">{ d.news_title }</h4>
-            <div className="news__item__text">{ d.news_text }</div>
-            <div><ImagesView data={ d.images }></ImagesView></div>
-            <div><VideosView data={ d.videos }></VideosView></div>
-            <div className="news__item__date">
-              { formatDate(new Date( d.news_dt ), false) }
-            </div>
-          </div>);
-        })
-      }
+      { data.map((d, i) => <NewsItem key={ i } item={ d } { ...props }/>) }
     </div>);
+}
+
+class NewsItem extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      opened: props.item.news_text.length > NEWS_VISIBLE_LENGTH ? false : true
+    }
+  }
+
+  render(){
+    var d = this.props.item;
+    return (<div className="news__item">
+
+      { this.props._admin ? <div className="admin__controls">
+            <Link className="admin__edit-button" onClick={ () => this.props._editClick(d) }></Link>
+            <Link className="admin__delete-button" onClick={ () => this.props._deleteClick(d) }></Link>
+          </div> : undefined }
+
+      <h4 className="news__item__title">{ d.news_title }</h4>
+      <div className={ 'news__item__text show-more-text' + ( this.state.opened ? '_opened' : '' ) }>
+        <span>{ this.state.opened ? d.news_text : d.news_text.slice(0, NEWS_VISIBLE_LENGTH) + '...' }</span>
+        <div className="show-more" onClick={ () => this.setState({ opened: true }) }></div>
+      </div>
+      <div><ImagesView data={ d.images }></ImagesView></div>
+      <div><VideosView data={ d.videos }></VideosView></div>
+      <div className="news__item__date">
+        { formatDate(new Date( d.news_dt ), false) }
+      </div>
+    </div>);
+  }
 }
